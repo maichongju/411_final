cd5d55a58b7584693ff8b38032fe9c58ef81fbe5
maichongju <31859452+maichongju@users.noreply.github.com>
2017-11-04 22:00:13 -0400

    Updated main.cpp
    Window will be center of the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,51 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+GLint winWidth = 800, winHeight = 800, winx = 100, winy = 100;
+
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glFlush();
+	glutSwapBuffers();
+}
+
+void winReshapeFun(GLint newWidth, GLint newHeight) {
+	glViewport(0, 0, newWidth, newWidth);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+}
+
+/**
+ * Function will set the windows to the center of the screen according screen resolution
+ * if screen_height or screen_width means the resolution cannot get correctly, then the
+ * function will not change the default value for window create position
+ * @param screen_height
+ * 			the height of the current screen, 0 means resolution unavailable
+ * @param screen_width
+ * 			the width of the current screen, 0 means resolution unavailable
+ */
+void centerwindow(int screen_height, int screen_width) {
+	if (screen_height != 0 && screen_width != 0) {
+		winx = screen_width / 2 - winWidth / 2;
+		winy = screen_height / 2 - winHeight / 2;
+	}
+}
+
 int main(int argc, char** argv) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	centerwindow(glutGet(GLUT_SCREEN_HEIGHT), glutGet(GLUT_SCREEN_WIDTH));
+	glutInitWindowPosition(winx, winy);
+	glutInitWindowSize(winWidth, winHeight);
+	glutCreateWindow("CP411 Final Project");
+	glutDisplayFunc(display);
+
+
+	glutMainLoop();
 
 	return 0;
 }
